76b8d0e616c1ba4950bfa9c4f13334963c3706ff
Evan Balzuweit <ebalzuweit@gmail.com>
2013-08-01 11:26:40 -0500

    Added database sql file

---
+++ schema/iclicker_base.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 01, 2013 at 06:23 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `iclicker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  `course_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(100) NOT NULL,
+  `course_number` varchar(10) NOT NULL,
+  PRIMARY KEY (`course_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `onlineresponses`
+--
+
+CREATE TABLE IF NOT EXISTS `onlineresponses` (
+  `question_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `response` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  `question_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `session_id` int(11) NOT NULL,
+  `question_number` int(11) NOT NULL,
+  `question_name` varchar(100) NOT NULL,
+  `screen_picture` varchar(100) NOT NULL,
+  `chart_picture` varchar(100) NOT NULL,
+  `correct_answer` varchar(10) NOT NULL,
+  `start_time` varchar(50) NOT NULL,
+  `stop_time` varchar(50) NOT NULL,
+  `ignore_question` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`question_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=116 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `responses`
+--
+
+CREATE TABLE IF NOT EXISTS `responses` (
+  `question_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `number_of_attempts` int(11) NOT NULL,
+  `first_response` varchar(10) NOT NULL,
+  `time` float NOT NULL,
+  `response` varchar(10) NOT NULL,
+  `final_answer_time` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sections`
+--
+
+CREATE TABLE IF NOT EXISTS `sections` (
+  `section_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `course_id` int(11) NOT NULL,
+  `section_number` int(11) NOT NULL,
+  `year_offered` int(11) NOT NULL,
+  PRIMARY KEY (`section_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `section_id` int(11) NOT NULL,
+  `session_date` varchar(100) NOT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE IF NOT EXISTS `students` (
+  `student_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `iclicker_id` varchar(50) NOT NULL,
+  `school_id` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
