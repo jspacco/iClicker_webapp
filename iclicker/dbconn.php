a7165cef8196e930725c4a5a53257bb9da7134e2
Austen Lake <alake@knox.edu>
2014-09-25 14:09:54 -0500

    Final commit probably

--- iclicker/dbconn.php
+++ iclicker/dbconn.php
@@ -8,7 +8,7 @@ function connect() {
         $dbhost = 'localhost';
         $dbuser = 'root';
         $dbpass = '';
-        $dbname = 'cs147';
+        $dbname = 'testdb1';
 
         $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die ('Error connecting to mysql ' . mysqli_connect_error());
         return $conn;
